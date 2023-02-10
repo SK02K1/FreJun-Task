85229ef8989f4166d6880234922c2d4b3a3c6bf8
Sourabh Kheraliya <55895224+SK02K1@users.noreply.github.com>
2023-02-10 14:03:51 +0530

    feat: logout user

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,10 +1,15 @@
+import { useAuth } from 'contexts';
 import './index.css';
 
 export const Navbar = () => {
+  const { setIsLoggedIn } = useAuth();
+  const logoutHandler = () => setIsLoggedIn(false);
   return (
     <nav className='navbar'>
       <h1 className='logo'>FreJun Task</h1>
-      <button className='btn btn-logout'>Logout</button>
+      <button onClick={logoutHandler} className='btn btn-logout'>
+        Logout
+      </button>
     </nav>
   );
 };
