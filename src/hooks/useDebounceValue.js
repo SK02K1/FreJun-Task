0948e8ac0dbc51caf1957198e7821d2c3bcb4181
SK02K1 <ksourabh458@gmail.com>
2023-02-11 02:24:04 +0530

    feat: debounced search

---
+++ src/hooks/useDebounceValue.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react';
+
+export const useDebounceValue = (value, delay = 400) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+};
